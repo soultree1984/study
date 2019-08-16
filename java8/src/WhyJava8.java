fbbcab7595668b381e33c6987acd5f0469dc91ed
soultree1984 <soulate@gmail.com>
2019-08-17 02:16:00 +0900

    Add projects

---
+++ java8/src/WhyJava8.java
@@ -0,0 +1,6 @@
+public class WhyJava8 {
+    public static void main(String[] args) {
+
+        System.out.println("Why Java8....");
+    }
+}
---
+++ oop-design-pattern/src/DesignPattern.java
@@ -0,0 +1,6 @@
+public class DesignPattern {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to design pattern");
+    }
+}
